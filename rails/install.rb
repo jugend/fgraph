85de9286216ced0051a9b1b0f59fed06e5bc5bb7
Herryanto Siatono <herryanto@gmail.com>
2010-05-26 17:25:09 +0800

    Move install.rb to base directory

--- rails/install.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'fileutil'
-
-fgraph_config = File.join(RAILS_ROOT, "config", "fgraph.yml")
-unless File.exist?(fgraph_config)
-  fgraph_config_template = File.join(RAILS_ROOT, "vendor", "plugins", "fgraph", "templates", "fgraph.yml")
-  FileUtils.cp fgraph_config_template, fgraph_config
-  puts "#{RAILS_ROOT}/config/fgraph.yml created, please update your app_id and app_secret."
-end
